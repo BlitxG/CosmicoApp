76638f2e55c2456d37b8e0ba10652feed6f20e30
Anishan Barathan <anishan9@outlook.com>
2021-08-02 21:02:48 +0100

    Break in code, testing sound functions

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
-alert('HELLO!!!')
+window.addEventListener('load', () => {
+    const sounds = document.querySelectorAll('.sound');
+    const pads = document.querySelectorAll('.pads div');
+    
+    console.log(sounds[0]);
+
+    // Sound code
+    pads.forEach(pad =>{
+        pad.addEventListener('click', function() {
+            sounds[4]
+        });
+    });
+});
+
+
